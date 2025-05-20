41c94d5c3a306f53e4de37e0fe747a365f9cab2a
ARSiddique <abdulraufsiddique10@gmail.com>
2025-05-21 00:37:05 +0500

    fixing buttons bugs

--- client/src/components/EmailButton.jsx
+++ client/src/components/EmailButton.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const EmailButton = () => (
     <Link
         to="/contact"
-        className="fixed bottom-20 left-4 bg-yellow-500 text-white p-3 rounded-full shadow-lg hover:bg-yellow-600 transition z-50"
+        className="fixed bottom-20 left-4 bg-yellow-500 text-white p-3 rounded-full shadow-lg hover:bg-yellow-600 transition z-[9999]"
         aria-label="Go to Contact Page"
     >
         <FaEnvelope size={20} />
